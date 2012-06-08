6f9972a709c065b2f23b32201fe78335fd860ed0
Edward Holland <edward.holland@csiro.au>
2012-06-08 16:02:04 +1000

    Adding script to test sensordb_api

---
+++ sensordb_test.py
@@ -0,0 +1,30 @@
+from sensordb_api import *
+import getpass
+
+default_host = "http://phenonet.com:9001"
+
+host = raw_input("Please enter the host you wish to connect to (None for default). [" + default_host + "]:")
+if host == "":
+    host = default_host
+
+username = raw_input("Username: ")
+password = getpass.getpass("Password: ")
+
+
+test_db = SensorDB(host, username, password)
+
+print vars(test_db.user).keys()
+
+print test_db.user.description
+
+for experiment in test_db.experiments:
+    print "Experiment Variables: " + vars(experiment).keys() 
+    print "Experiment ID: " + experiment._id
+    print "Experiment Name: " + experiment.name
+    print
+
+
+
+
+
+

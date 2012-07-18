72b0f2eb167887a227fa712552aaffc4d1e9c063
Edward Holland <edward.holland@csiro.au>
2012-07-18 11:21:17 +1000

    Updated smartcrop upload script

--- tests/Smartcrop2011/upload_data.py
+++ tests/Smartcrop2011/upload_data.py
@@ -17,17 +17,15 @@ sensor_list_file = "R:/MCWheat/Equipment/SmartCrop/data2011/SmartField/SmartFiel
 ''' Connect to SensorDB '''
 sensor_db = SensorDB(host, username, password)
 
-
-
-
-
 ''' Create experiments ''' 
 # Read in the sensor list
-sensor_list = read_csv(sensor_list_file)
+sensor_list = read_csv(sensor_list_file, parse_dates = [4, 5], dayfirst = True)
 
 # Create a list of unique experiment names
 experiment_names = set()
 for experiment in sensor_db.experiments:
+    # DEBUG - delete experiment
+    #experiment.delete()
     experiment_names.add(experiment.name)
 
 ''' Note - This code is written assuming that a single trial is only ever on a single site'''
@@ -43,7 +41,7 @@ for trial in sensor_list["TrialCode"].unique():
     print sites
     
     if sites.size > 1:
-        print "\"" + trial + "\" has multiple sites. This is not yet handled. Only the first site will be used"
+        print "\"" + trial + "\" has multiple sites. This is not currently handled. Only the first site will be used"
     
     
     # Skip this trial if it already exists as an experiment
@@ -52,9 +50,37 @@ for trial in sensor_list["TrialCode"].unique():
     
     # TODO - determine the timezone from the site. This would require a dictionary of sites.
     
-    print sensor_db.user.create_experiment(trial, "Australia/Brisbane")
+
+    experiment = sensor_db.user.create_experiment(trial, "Australia/Brisbane")
     
-    # TODO - Find the experiment and add the site as metadata
+    # Add the site/s as metadata
+    experiment.metadata_add("site", sites[0])
+    #experiment.metadata_add("Sites", str(sites))
+
     
-    pass
+''' Add nodes to experiments '''
+
+sensor_db.get_session()
+
+existing_nodes = dict()
+
+for experiment in sensor_db.experiments:
+    existing_nodes[experiment.name] = set()
+    
+    for node in experiment.nodes:
+        existing_nodes[experiment.name].add(node.name)
+
+for node_index in sensor_list.index:
+    # Name should be Genotype, Range, Plot. e.g. ABC-R01P01
+    node_name = sensor_list["Genotype"][node_index] + "-R{:02}".format(sensor_list["Range"][node_index]) + "P{:02}".format(sensor_list["Plot"][node_index])
+    
+    # skip existing added nodes
+    if node_name in existing_nodes:
+        continue
+    
+    
+    
+    existing_nodes.add(node_name)
+    
+    
     

ef5ae60a3bffb53968f474ef2fbb3a2c5ca4c591
Edward Holland <edward.holland@csiro.au>
2012-09-05 16:52:49 +1000

    Adding delete experiment test

---
+++ tests/delete_test.py
@@ -0,0 +1,20 @@
+
+sys.path.append('../..')
+
+from sensordb_api import *
+
+host = "http://phenonet.com:9001"
+username = "testUser"
+password = "password"
+
+''' Connect to SensorDB '''
+print "Connecting to Database"
+sensor_db = SensorDB(host, username, password)
+
+print "Clearing Experiments"
+for experiment in sensor_db.experiments:
+    # DEBUG - delete experiment
+    experiment.delete()
+
+
+experiment = sensor_db.user.create_experiment("Experiment Name", "Australia/Queensland")
